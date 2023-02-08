beeac971c0839b1ddbf089e6212ea143a3042f38
anwar-2000 <khaledkhaouani5@gmail.com>
2023-02-08 00:11:41 +0000

    simple manual form data submitting

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def review(request):
+    if request.method=='POST': #checking the  HTTP method
+        entered_username = request.POST['username']
+        return HttpResponseRedirect('thank-you')
     return render(request,"reviews/index.html")
+
+def thankyou(request):
+    return render(request,'reviews/thankyou.html')
